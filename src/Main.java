56958c2f0a8aead7f95443e06388de949d5e7dbb
Ilya Sergeevtsev <il.ser0304@gmail.com>
2025-11-09 19:11:15 +0300

    add calculation

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,18 @@ import java.util.Scanner;
 
 public  class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста " + text.length());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int sum = firstNumber + secondNumber;
+        int rest = firstNumber - secondNumber;
+        int mul = firstNumber * secondNumber;
+        double quotient = firstNumber / secondNumber;
+
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разница :" + rest);
+        System.out.println("Произведение: " + mul);
+        System.out.println("Частное: " + quotient);
     }
 }
